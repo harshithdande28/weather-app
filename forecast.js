ec909f2a4241ff30319af9549897d9d7e3bfeeed
harshithdande <152005744+harshithdande28@users.noreply.github.com>
2024-04-29 16:06:13 +0530

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -1,17 +1,17 @@
 import request from "postman-request";
 const rgc = (lat,lon,callback)=>{
     const url = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&'+'lon='+lon+'&appid=YOUR API-KEY GOES HERE'
-request({url,json: true},(error,response)=>{ // (error,{body}) this is destructuring of the object response
+request({url,json: true},(error,response)=>{ 
     if(error){
         callback('Unable to connect!',undefined)
-    }else if(response.body.error || response.body.cod === '200'){ //(body.error || body.cod)
+    }else if(response.body.error || response.body.cod === '200'){ 
         callback('Unable to find location!',undefined)
     }
     else{
         callback(undefined,{
-            name:response.body.name, //body.name
-            main:response.body.main, //body.main
-            country: response.body.sys.country //body.sys.country
+            name:response.body.name, 
+            main:response.body.main, 
+            country: response.body.sys.country 
         })
     }
 })}
